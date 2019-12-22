bcadaa32f337e12c4fadd10c1ca8ced891f14b57
MinexAutomation <minex.automation@gmail.com>
2019-12-21 22:24:03 -0800

    Initial commit of source-stub.

---
+++ source/Testing.Repository/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Testing.Repository
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
